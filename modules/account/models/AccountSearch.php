09888eca0e76823a9e1b3de8432370c75a521793
Danil <90402467+Danlog222@users.noreply.github.com>
2024-06-18 18:09:00 +0300

    generate accound and admin

---
+++ modules/account/models/AccountSearch.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace app\modules\account\models;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use app\models\Application;
+use Yii;
+
+/**
+ * AccountSearch represents the model behind the search form of `app\models\Application`.
+ */
+class AccountSearch extends Application
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'user_id', 'status_id', 'master_id'], 'integer'],
+            [['date', 'created_at', 'description'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Application::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+            'pagination' => [
+                'pageSize' => 4
+            ],
+            'sort' => [
+                'defaultOrder' => [
+                    'created_at' => SORT_DESC,
+                ]
+            ],
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'user_id' => Yii::$app->user->identity->id,
+            'status_id' => $this->status_id,
+            'date' => $this->date,
+            'created_at' => $this->created_at,
+            'master_id' => $this->master_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'description', $this->description]);
+
+        return $dataProvider;
+    }
+}
---
+++ modules/account/views/account/_search.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\modules\account\models\AccountSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="application-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'user_id') ?>
+
+    <?= $form->field($model, 'status_id') ?>
+
+    <?= $form->field($model, 'date') ?>
+
+    <?= $form->field($model, 'created_at') ?>
+
+    <?php // echo $form->field($model, 'description') ?>
+
+    <?php // echo $form->field($model, 'master_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
